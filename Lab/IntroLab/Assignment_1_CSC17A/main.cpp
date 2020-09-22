b534a1705a98fec641c6092422c55277696f02f9
ce2791334 <cengland2@student.rccd.edu>
2020-09-22 08:22:50 -0700

    Signed-off-by: ce2791334 <cengland2@student.rccd.edu>

---
+++ Lab/IntroLab/Assignment_1_CSC17A/main.cpp
@@ -0,0 +1,17 @@
+/* Cody England
+ CSC-17A_49287
+ Assignment_1_HelloWorld
+ 08/27/20*/
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(int argc, char** argv) {
+
+    cout << "Hello World! \nThis is my first 17A Program." << endl;
+    
+    return 0;
+}
+
